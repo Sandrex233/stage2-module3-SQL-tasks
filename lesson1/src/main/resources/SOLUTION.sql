198052430cf41eebf9f255c0dd6b1babcd850286
Sandrex233 <sandrex233@gmail.com>
2023-02-26 14:46:23 +0400

    fix

--- lesson1/src/main/resources/SOLUTION.sql
+++ lesson1/src/main/resources/SOLUTION.sql
@@ -1,42 +1,42 @@
-CREATE TABLE Student
+CREATE TABLE student
 (
-    id        BIGINT PRIMARY KEY,
-    name      VARCHAR(255),
-    birthday  DATE,
-    group_num INT
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(45),
+    birthday DATE NOT NULL,
+    groupnumber INT NOT NULL
 );
-
-CREATE TABLE Subject
+CREATE TABLE subject
 (
-    id          BIGINT PRIMARY KEY,
-    name        VARCHAR(255),
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(250),
     description VARCHAR(255),
-    grade       INT
+    grade int NOT NULL
 );
 
-CREATE TABLE PaymentType
+CREATE TABLE mark
 (
-    id   BIGINT PRIMARY KEY,
-    name VARCHAR(255)
-);
-
-CREATE TABLE Payment
-(
-    id           BIGINT PRIMARY KEY,
-    type_id      BIGINT,
-    amount       DECIMAL,
-    student_id   BIGINT,
-    payment_date DATETIME,
-    FOREIGN KEY (type_id) REFERENCES PaymentType (id),
-    FOREIGN KEY (student_id) REFERENCES Student (id),
-);
-
-CREATE TABLE Mark
-(
-    id         BIGINT PRIMARY KEY,
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     student_id BIGINT,
     subject_id BIGINT,
-    mark       INT,
-    FOREIGN KEY (student_id) REFERENCES Student (id),
-    FOREIGN KEY (subject_id) REFERENCES Subject (id),
+    mark INT NOT NULL,
+    foreign key (student_id) references student(id),
+    foreign key (subject_id) references subject(id)
 );
+
+CREATE TABLE paymenttype
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(45)
+);
+
+CREATE TABLE payment
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    type_id BIGINT NOT NULL,
+    amount decimal NOT NULL,
+    payment_date TIMESTAMP  NOT NULL,
+    student_id BIGINT NOT NULL,
+    foreign key (type_id) references paymenttype(id),
+    foreign key (student_id) references student(id)
+);
+
